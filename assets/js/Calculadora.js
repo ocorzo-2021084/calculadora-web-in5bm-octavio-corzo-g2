aa11264858da9af51e9d8f61578a4b2382b0971b
Octavio Corzo <ocorzo-2021084@kinal.edu.gt>
2022-07-23 00:06:34 -0600

    Subiendo el proyecto de la calculadora web

---
+++ assets/js/Calculadora.js
@@ -0,0 +1,33 @@
+class Calculadora {
+    sumar(num1, num2) {
+        return num1 + num2;
+    }
+
+    restar(num1, num2) {
+        if((num1 -num2)==0){
+            return '0';
+        }else{
+            return num1 - num2
+        }
+    }
+
+    dividir(num1, num2) {
+        return num1 / num2;
+    }
+
+    multiplicar(num1, num2) {
+        if((num2)==0){
+            return '0';
+        }else{
+            return num1 * num2;
+        }
+    }
+
+    residuo(num1, num2){
+        if((num1 % num2)==0){
+            return '0';
+        }else{
+            return num1 % num2;
+        }
+    }
+} 
